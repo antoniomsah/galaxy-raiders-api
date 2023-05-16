0a591dc31f2da0065208c2cca65ccd98229134c5
antoniomsah <amsah@usp.br>
2023-05-16 11:32:52 -0300

    Implemented most Point2D methods and attributes

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -1,13 +1,12 @@
-@file:Suppress("UNUSED_PARAMETER") // <- REMOVE
 package galaxyraiders.core.physics
 
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+	return Point2D(x=this.x+p.x, y=this.y+p.y);
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    return Point2D(x=this.x+v.dx, y=this.y+v.dy);
   }
 
   override fun toString(): String {
@@ -15,15 +14,15 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx=this.x,dy=this.y);
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx=p.x-this.x, dy=p.y-this.y);
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return (this.impactVector(p)).unit
   }
 
   fun contactVector(p: Point2D): Vector2D {
@@ -35,6 +34,6 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+	return Vector2D(dx=p.x-this.x, dy=p.y-this.y).magnitude;
   }
 }
